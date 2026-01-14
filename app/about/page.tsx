589304a0560bd51da1625e09746b50b5eb3a12b3
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-15 00:24:14 +0600

    clear

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,12 @@
-export default function Blogpage(){
+export const dynamic = "force-dynamic"
+
+
+export default async function Blogpage(){
+
+    await new Promise((resolve)=> setTimeout(resolve, 4000))
+
+    throw new Error("Something went wrong")
+
     return(
         <div>
         <h1>This is About page component</h1>
