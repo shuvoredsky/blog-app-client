84eebb5c56533f10026048962c1c31c20f42a3ee
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-18 23:44:30 +0600

    update by shuvo

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,12 @@
+import { authClient } from "@/lib/auth-client";
 import { Button } from "../../components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = authClient.getSession();
+
+  console.log("console from page .tssx", session)
+
   return (
     <div>
       <Button variant="outline">Click Here</Button>
