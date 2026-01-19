c64ec4162cdc3cbc669526d576b6c163486c3b21
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-20 00:54:07 +0600

    blog service

---
+++ src/services/blog.service.ts
@@ -0,0 +1,16 @@
+import { env } from "@/env";
+
+const API_URL = env.API_URL;
+
+export const blogService = {
+    getBlogPosts: async function () {
+        try {
+            const res = await fetch(`${API_URL}/posts`);
+            const data = await res.json();
+
+            return {data: data, error: null}
+        } catch(err){
+            return {data: null, error: {message: "something went wrong"}}
+        }
+    }
+}
