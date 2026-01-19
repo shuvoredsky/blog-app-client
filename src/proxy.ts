3e279a565ca406fbf4ff32ad7c0c816474632344
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-20 00:21:42 +0600

    privet route

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { userService } from "./services/user.services";
+import { Roles } from "./constants/role";
 
 export async function proxy (request: NextRequest){
     
+    const pathname = request.nextUrl.pathname;
+
     let isAuthenticated = false;
     let isAdmin = false;
     const {data} = await userService.getSession()
 
     if(data){
         isAuthenticated = true;
-        isAdmin = data.user.role
+        isAdmin = data.user.role == Roles.admin;
     }
+
+        if(!isAuthenticated){
+            return NextResponse.redirect(new URL("/login", request.url))
+        }
+
+
+        if(isAdmin && pathname.startsWith("/dashboard")){
+            return NextResponse.redirect(new URL("/admin-dashboard", request.url))
+        }
+
+        if(!isAdmin && pathname.startsWith("/admin-dashboard")){
+            return NextResponse.redirect(new URL("/dashboard", request.url))
+        }
+
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard"]
+    matcher: ["/dashboard", "/dashboard/:path*", "/admin-dashboard","/admin-dashboard/:path*"]
 }
